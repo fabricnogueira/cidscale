cddd5f060a3ad9c7b5d4d9635b3382d5be7b0477
fabricnogueira <fabricio221@gmail.com>
2024-10-23 13:52:31 -0300

    Refactor routes.rb to remove unnecessary get action and add 'calcular' post action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-
   root 'principal#index'
-
-  get 'principal/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'calcular', to: 'principal#calcular'
 end
